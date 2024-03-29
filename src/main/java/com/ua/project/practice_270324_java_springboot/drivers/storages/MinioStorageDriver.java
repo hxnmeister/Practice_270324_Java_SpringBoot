39eb4d310a21b5b7b3e15e5dbaf3a13eaeee0597
Valerii <valeriy.stronskiy@gmail.com>
2024-03-29 11:15:09 +0200

    Minio driver

--- src/main/java/com/ua/project/practice_270324_java_springboot/drivers/storages/MinioStorageDriver.java
+++ src/main/java/com/ua/project/practice_270324_java_springboot/drivers/storages/MinioStorageDriver.java
@@ -1,10 +1,55 @@
 package com.ua.project.practice_270324_java_springboot.drivers.storages;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public class MinioStorageDriver implements StorageDriver {
+    private MinioClient minioClient;
+
+    public MinioStorageDriver(String minioUrl, String user, String password) {
+        minioClient = MinioClient
+                .builder()
+                .endpoint(minioUrl)
+                .credentials(user, password)
+                .build();
+    }
+
     @Override
     public String put(String bucketName, String fileName, MultipartFile file) {
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(file.getBytes())) {
+            final int PART_SIZE = -1;
+
+            createBucketIfNotExists(bucketName);
+
+            minioClient.putObject(PutObjectArgs
+                    .builder()
+                    .bucket(bucketName)
+                    .object(fileName)
+                    .stream(byteArrayInputStream, byteArrayInputStream.available(), PART_SIZE)
+                    .contentType(file.getContentType())
+                    .build());
+
+            return fileName;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         return "";
+    }
+
+    private void createBucketIfNotExists(String bucketName) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        if(!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        }
     }
 }
--- src/main/java/com/ua/project/practice_270324_java_springboot/services/StorageService.java
+++ src/main/java/com/ua/project/practice_270324_java_springboot/services/StorageService.java
@@ -1,6 +1,7 @@
 package com.ua.project.practice_270324_java_springboot.services;
 
 import com.ua.project.practice_270324_java_springboot.drivers.storages.LocalStorageDriver;
+import com.ua.project.practice_270324_java_springboot.drivers.storages.MinioStorageDriver;
 import com.ua.project.practice_270324_java_springboot.drivers.storages.StorageDriver;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,11 +13,21 @@ public class StorageService {
     @Value("${storage.local.path}")
     private String localStoragePath;
 
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Value("${minio.user}")
+    private String minioUser;
+
+    @Value("${minio.password}")
+    private String minioPassword;
+
     private StorageDriver storageDriver;
 
     @PostConstruct
     public void initialize() {
-        storageDriver = new LocalStorageDriver(localStoragePath);
+//        storageDriver = new LocalStorageDriver(localStoragePath);
+        storageDriver = new MinioStorageDriver(minioUrl, minioUser, minioPassword);
     }
 
     public String put(String bucketName, String fileName, MultipartFile file) {
